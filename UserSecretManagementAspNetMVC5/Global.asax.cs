6524780c84d4e2d28f3c19ef247437c637c6ea73
Hamid Mosalla <Xellarix@gmail.com>
2022-12-30 18:20:08 +0100

    install the secret config package and put in place the code and secrets to read form them

--- UserSecretManagementAspNetMVC5/Global.asax.cs
+++ UserSecretManagementAspNetMVC5/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,10 +13,20 @@ namespace UserSecretManagementAspNetMVC5
     {
         protected void Application_Start()
         {
+            var values = ReadFromSecretFile();
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private (string identityServer4ClientId, string identityServer4ClientSecret) ReadFromSecretFile()
+        {
+            var clientId = ConfigurationSettings.AppSettings.Get("IdentityServer4ClientId");
+            var clientSecret = ConfigurationSettings.AppSettings.Get("IdentityServer4ClientSecret");
+
+            return (clientId, clientSecret);
         }
     }
 }
